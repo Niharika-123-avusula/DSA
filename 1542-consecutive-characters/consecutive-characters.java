31ca2abbd646835f73cdb6f7b42164e7a82b023c
Niharika-123-avusula <niharikaavusula8@gmail.com>
2025-09-09 16:25:09 +0530

    Time: 1 ms (99.69%) | Memory: 41.9 MB (89.54%) - LeetSync

---
+++ 1542-consecutive-characters/consecutive-characters.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxPower(String s) {
+        int i=0,j=1;
+        int len=Integer.MIN_VALUE;
+        while(j<s.length()){
+            if(s.charAt(i)==s.charAt(j)){
+                len=Math.max(len,j-i+1);
+                j++;
+            }
+            else{
+                i=j;
+                j++;
+            }
+        }
+       
+        return len==Integer.MIN_VALUE?1:len;
+    }
+}
